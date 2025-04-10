23dec469c1d778500c92dd7cbead1102f07be472
p-rosit <pontus.rosqvist@gmail.com>
2025-04-10 19:57:30 +0200

    ensure ints are terminated by newline

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -683,6 +683,7 @@ bool lec_internal_lexer_number_valid_end(struct LecLexer *lexer, char c) {
         case '!':
         case '*':
         case '/':
+        case '\\':
         case '=':
         case '(':
         case ')':
